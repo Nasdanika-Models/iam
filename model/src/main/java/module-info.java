6f855ffb2aa6be6f732955cf40fcc93b1a14db05
Pavel Vlasov <Pavel.Vlasov@nasdanika.org>
2026-08-14 07:38:44 -0400

    Dependency cleanup, copying xcore model to the site root, diagram

--- model/src/main/java/module-info.java
+++ model/src/main/java/module-info.java
@@ -2,16 +2,12 @@ import org.nasdanika.capability.CapabilityFactory;
 import org.nasdanika.models.iam.capability.IamEPackageResourceSetCapabilityFactory;
 import org.nasdanika.models.iam.capability.IamResourceFactoryCapabilityFactory;
 
-module org.nasdanika.models.iam.Iam {
+module org.nasdanika.models.iam {
 	
 	exports org.nasdanika.models.iam;
 	exports org.nasdanika.models.iam.impl;
 	exports org.nasdanika.models.iam.util;
-	
-	requires transitive org.eclipse.emf.ecore;
-	requires transitive org.eclipse.emf.common;
-	requires transitive org.nasdanika.capability;
-	
+		
 	requires transitive org.nasdanika.models.role;
 	
 	provides CapabilityFactory with 
